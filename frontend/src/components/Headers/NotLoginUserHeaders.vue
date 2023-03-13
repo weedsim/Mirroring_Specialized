2d60f11f3a150d831c58a642066301a5d215e2d0
JGW <wrd4000@naver.com>
2023-03-13 15:14:22 +0900

    :sparkles: header 1차 완성본

--- frontend/src/components/Headers/NotLoginUserHeaders.vue
+++ frontend/src/components/Headers/NotLoginUserHeaders.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="navbar">
     <span>
-      <a href="/">
+      <a href="/" class="headerlist">
         <img src="@\assets\logo.png" alt="로고" class="logo">FANFTASY
       </a>
     </span>
     <span class="headerlist">
-      | <router-link to="/" style="text-decoration: none;">NFT 마켓</router-link> |
-      <router-link to="/about" style="text-decoration: none;">커뮤니티</router-link> |
+      <router-link to="/" style="text-decoration: none;">NFT 마켓</router-link> |
+      <router-link to="/about" style="text-decoration: none;">커뮤니티</router-link>
     </span>
-    <label for="" class="box" style="margin-left:300px;">
-      <input type="text" class="notting" style="width: 350px; height: 40px; display: flex; justify-content: left; float: left;">
+    <label for="" class="box" >
+      <input type="text" class="notting search-input">
       <button href="" class="notting" style="display: flex; justify-content: right; align-self : center;">
         <img src="@/assets/Search.png" alt="" style="margin-top: 7px;">
       </button>
     </label>
-    <span>
-      <a href="">
-        <img src="@\assets\wallet.png" alt="지갑" style="margin-right: 20px;">
+    <span style="margin-left: 10vw">
+      <a href="" class="icon-margin">
+        <img src="@\assets\wallet.png" alt="지갑">
       </a>
-      <a href="">
-        <img src="@\assets\base_profile.png" alt="기본 프로필 사진" style="margin-right: 20px;"> 
+      <a href="" class="icon-margin">
+        <img src="@\assets\base_profile.png" alt="기본 프로필 사진"> 
       </a>
     </span>
   </div>
@@ -34,7 +34,7 @@ export default {
 
 <style>
 .navbar {
-  height: 50px;
+  height: 70px;
   display: flex;
   align-items: center;
 }
@@ -45,7 +45,7 @@ export default {
 }
 
 .headerlist {
-  font-size: medium;
+  font-size: large;
   text-decoration: none;
 }
 
@@ -61,4 +61,16 @@ export default {
   border: none;
 }
 
+.search-input {
+  width: 350px;
+  height: 39px;
+  border-radius: 15px;
+  display: flex;
+  justify-content: left;
+  float: left;
+}
+
+.icon-margin {
+  margin-right: 2vw;
+}
 </style>
