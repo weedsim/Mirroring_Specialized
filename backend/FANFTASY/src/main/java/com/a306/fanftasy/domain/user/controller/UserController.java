346b3f6e9186254360ffd0e82ba91616859fd9c0
minsu121x <whalstn1020@hanmail.net>
2023-03-17 17:56:55 +0900

    :sparkles: security + metamask login 구현중

--- backend/FANFTASY/src/main/java/com/a306/fanftasy/domain/user/controller/UserController.java
+++ backend/FANFTASY/src/main/java/com/a306/fanftasy/domain/user/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.a306.fanftasy.domain.user.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/user")
+@Slf4j
 public class UserController {
+
+    public ResponseEntity<?> login(String walletID){
+
+        return null;
+    }
 
 }
