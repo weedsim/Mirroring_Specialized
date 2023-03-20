fc4d5fe787d1e29cabee1d4e2c5890619330730b
JGW <wrd4000@naver.com>
2023-03-20 18:00:53 +0900

    :sparkles: NFT Market Card 추가

---
+++ frontend/src/components/market/NFTCard.vue
@@ -0,0 +1,115 @@
+<template>
+  <div >
+    <div style="display: inline-block;">
+      <v-card class="nft-card">      
+        <v-card-title style="padding: 0; display: flex; justify-content: center; position: relative;" >
+          <v-img src="@/assets/뉴진스.png" alt="" class="card-img"></v-img>
+        </v-card-title>
+        <v-card-item style="font-size:larger; vertical-align: center; text-align: center; ">
+          <p class="item-name">
+            아티스트
+          </p>
+          <p class="item-content">
+            심호연
+          </p>
+          <p class="item-name">
+            제목
+          </p>
+          <p class="item-content">
+            코인 손해본거 놀릴 때 호연
+          </p>
+          <p class="item-name">
+            가격
+          </p>
+          <p class="item-content">
+            1.03FAN
+          </p>
+          <p class="item-name">
+            잔여수량
+          </p>
+          <p class="item-content">
+          19/50
+          </p>
+        </v-card-item>
+      </v-card>
+      <v-card class="nft-card">      
+        <v-card-title style="padding: 0; display: flex; justify-content: center; position: relative;" >
+          <v-img src="@/assets/뉴진스.png" alt="" class="card-img"></v-img>
+        </v-card-title>
+        <v-card-item style="font-size:larger; vertical-align: center; text-align: center; ">
+          <br>
+          New Jeans
+        </v-card-item>
+      </v-card>
+      <v-card class="nft-card">      
+        <v-card-title style="padding: 0; display: flex; justify-content: center; position: relative;" >
+          <v-img src="@/assets/뉴진스.png" alt="" class="card-img"></v-img>
+        </v-card-title>
+        <v-card-item style="font-size:larger; vertical-align: center; text-align: center; ">
+          <br>
+          New Jeans
+        </v-card-item>
+      </v-card>
+    </div>
+    <br>
+    <div style="display: inline-block;">
+      <v-card class="nft-card">      
+        <v-card-title style="padding: 0; display: flex; justify-content: center; position: relative;" >
+          <v-img src="@/assets/뉴진스.png" alt="" class="card-img"></v-img>
+        </v-card-title>
+        <v-card-item style="font-size:larger; vertical-align: center; text-align: center; ">
+          <br>
+          New Jeans
+        </v-card-item>
+      </v-card>
+      <v-card class="nft-card">      
+        <v-card-title style="padding: 0; display: flex; justify-content: center; position: relative;" >
+          <v-img src="@/assets/뉴진스.png" alt="" class="card-img"></v-img>
+        </v-card-title>
+        <v-card-item style="font-size:larger; vertical-align: center; text-align: center; ">
+          <br>
+          New Jeans
+        </v-card-item>
+      </v-card>
+      <v-card class="nft-card">      
+        <v-card-title style="padding: 0; display: flex; justify-content: center; position: relative;" >
+          <v-img src="@/assets/뉴진스.png" alt="" class="card-img"></v-img>
+        </v-card-title>
+        <v-card-item style="font-size:larger; vertical-align: center; text-align: center; ">
+          <br>
+          New Jeans
+        </v-card-item>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NFTCard",
+
+}
+</script>
+
+<style>
+.nft-card {
+  display: flex;
+  float: left;
+  width: 413px;
+  height: 245px;
+  border: solid 1px #6a3fc1;
+}
+
+.item-name{
+  color: #6A3FC1;
+  text-align: left;
+  font-size: 15px;
+  font-weight: bolder;
+}
+
+.item-content{
+  text-align: left;
+  margin-left: 1vw;
+  font-size: 15px;
+}
+</style>
--- frontend/src/views/market/MarketListView.vue
+++ frontend/src/views/market/MarketListView.vue
@@ -1,13 +1,86 @@
 <template>
-  <div></div>
+  <br>
+  <br>
+  <div>
+    <div class="nftbar">
+      <span style="display: inline-block">
+        <h1 class="market-space">판매중인 NFT</h1>
+        
+        <button v-on:click="selectNewest" v-if="sortnum === 0" class="selected-ranking-button">최신순</button>
+        <button v-on:click="selectNewest" v-else class="unselected-ranking-button">최신순</button>
+        <button v-on:click="selectSalesVolume" v-if="sortnum === 1" class="selected-ranking-button">
+          판매량순
+        </button>
+        <button v-on:click="selectSalesVolume" v-else class="unselected-ranking-button">
+          판매량순
+        </button>
+        <button v-on:click="selectSalesAmmount" v-if="sortnum === 2" class="selected-ranking-button">
+          판매금액순
+        </button>
+        <button v-on:click="selectSalesAmmount" v-else class="unselected-ranking-button">
+          판매금액순
+        </button>
+      </span>
+      <label for="" class="box" style="display: inline-block">
+          <input type="text" class="notting search-input" />
+          <button
+            href=""
+            class="notting"
+            style="display: flex; justify-content: right; align-self: center"
+          >
+            <img
+              :src="require('@/assets/Search.png')"
+              alt=""
+              style="margin-top: 7px"
+            />
+          </button>
+        </label>
+    </div>
+    <NFTCard />
+  </div>
 </template>
 
 <script>
-export default {
+import NFTCard from "@/components/market/NFTCard.vue"
 
+export default {
+  name: "MarketListView",
+  components: {
+    NFTCard,
+  },
+  data() {
+    return {
+      sortnum: 0,
+    }
+  },
+  methods: {
+    selectNewest() {
+      this.sortnum = 0
+    },
+    selectSalesAmmount() {
+      this.sortnum = 2
+    },
+    selectSalesVolume() {
+      this.sortnum = 1
+    },
+  }
 }
 </script>
 
 <style>
+.nftbar {
+  display: flex;
+  justify-content: space-around;
+  /* white-space: nowrap; */
+  /* width: 100%; */
+  overflow: hidden;
+}
 
+.market-space {
+  display: flex;
+  justify-content: left;
+  /* margin-left: 7vw; */
+  margin-right: 3vw;
+  float: left;
+}
 </style>
