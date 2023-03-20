fdfe19371b6915d91a464e968601bb40145e3118
sin10071 <sinsh7001@gmail.com>
2023-03-20 16:35:23 +0900

    :sparkles: methods로 게시글 타입에 맞춰 필터링하여 출력하도록 코드 추가

--- frontend/src/views/community/CommunityMainView.vue
+++ frontend/src/views/community/CommunityMainView.vue
@@ -5,10 +5,10 @@
   <div class="communityMenu">
     <input type="text" placeholder="글 검색">
     <div class="communityMenu">
-      <div>전체보기</div>
-      <div>일반글</div>
-      <div>팝니다</div>
-      <div>삽니다</div>
+      <input type="button" value="전체보기" @click="callAll" style="margin-right: 10px;">
+      <input type="button" value="일반글" @click="callGeneral" style="margin-right: 10px;">
+      <input type="button" value="삽니다" @click="callPurchase" style="margin-right: 10px;">
+      <input type="button" value="팝니다" @click="callSales" style="margin-right: 10px;"> 
     </div>
   </div>
   <hr>
@@ -51,7 +51,11 @@
   </table>
 
   <!-- 페이지네이션 -->
-  
+  <div class="btn-cover">
+    <button :disabled="pageNum===0" @click="prevPage" class="page-btn">이전</button>
+    <span class="page-count"> {{pageNum+1}} / {{pageCount}} 페이지 </span>
+    <button :disabled="pageNum >= pageCount-1" @click="nextPage" class="page-btn">다음</button>
+  </div>
 
   <button class="writeButton">
     글쓰기
@@ -67,29 +71,125 @@ export default {
   name: "communityMain",
   data() {
     return {
-
-      articles : [
+// 버튼을 누르면 computed로 type별 필터링해서 articles를 반환하고 그걸 토대로 테이블에 출력하면 된다... 어떻게??
+      articles: [],
+      // articlesData : axios로 불러오는 데이터
+      articlesData : [
         { thumbnail: 'ImageGallery.png',
-        title:'제가 산 NFT 봐주세요',
+        type: '일반글',
+        title:'제가 산 NFT 좀 제발 봐주세요',
         writer: '조민수',
         createdAt: '2023-03-02',
         viewed: 12,
       },
       { thumbnail: 'ImageGallery.png',
+        type: '팝니다',
         title:'[팝니다] 현제 NFT 팝니다',
         writer: '조민수',
         createdAt: '2023-03-02',
-        viewed: 62,
+        viewed: 32,
+      },
+      { thumbnail: 'ImageGallery.png',
+        type: '일반글',
+        title:'제가 산 NFT 봐주세요',
+        writer: '조민수',
+        createdAt: '2023-03-02',
+        viewed: 45,
       },
       { thumbnail: 'Document.png',
+        type: '삽니다',
         title:'[삽니다] 호연 NFT 갖고 계신분 계신가요...',
         writer: '조민수',
         createdAt: '2023-03-02',
         viewed: 62,
       },
+      { thumbnail: 'Document.png',
+        type: '일반글',
+        title:'A306 이번 NFT 살만할까요...',
+        writer: '조민수',
+        createdAt: '2023-03-02',
+        viewed: 145,
+      },
     ],
-
+    // 페이지네이션용 데이터
+    listArray:[],
+    pageNum: 0,
+    pageSize: 3
     }
+  },
+  mounted() {
+    // axios({
+    //     method: 'get',
+    //     url: `/api/board/${type}/${keyword}`
+    //   })
+    //   .then((res)=>{
+    //     console.log(res)
+    //     this.articleData = res.data
+    //   })
+    //   .catch((err)=>{
+    //     console.log(err)
+    //   })
+    this.articles = this.articlesData
+  },  
+  methods:{
+    callAll(){
+      this.articles = this.articlesData
+    },
+    callGeneral(){
+      this.articles = this.articlesData
+      const newArticles = []
+      this.articles.filter((article)=>{
+        if (article.type==='일반글'){
+          newArticles.push(article)
+        }
+      })
+      this.articles = newArticles
+    },
+    callPurchase(){
+      this.articles = this.articlesData
+      const newArticles = []
+      this.articles.filter((article)=>{
+        if (article.type==='삽니다'){
+          newArticles.push(article)
+        }
+      })
+      this.articles = newArticles
+    },
+    callSales(){
+      this.articles = this.articlesData
+      const newArticles = []
+      this.articles.filter((article)=>{
+        if (article.type==='팝니다'){
+          newArticles.push(article)
+        }
+      })
+      this.articles = newArticles
+    },
+    nextPage(){
+      this.pageNum+=1;
+    },
+    prevPage(){
+      this.pageNum-=1;
+    }
+  },
+  computed: {
+    // pageCount(){
+    //   const aray = this.listArray.concat(this.articles1)
+    //   let listLeng = aray.length;
+    //   let listSize = this.pageSize;
+    //   let page = Math.floor((listLeng-1)/listSize)+1;
+    //   return page;
+    // },
+    // articles1(){
+    //   const aaa = this.$store.state.articles.slice().reverse() 
+    //   return aaa
+    // },
+    // pagenatedData(){
+    //   const aray = this.listArray.concat(this.articles)
+    //   const start = this.pageNum * this.pageSize;
+    //   const end = start + this.pageSize;  
+    //   return aray.slice(start,end);
+    // }
   },
   components: {
     LoginUserHeaders,
@@ -101,6 +201,10 @@ export default {
 <style>
   .communityMenu{
     display: flex;
+  }
+
+  .th {
+    padding: 0px 100px;
   }
 
   .writeButton{
@@ -115,6 +219,20 @@ export default {
     border-radius: 15px;
     color: white;
     background-color: RGB(106, 63, 193);
+  }
+  .btn-cover{
+  margin-top: 10px;
+  margin-bottom: 10px;
+  }
+  .btn-cover .page-btn{
+    width: 5rem;
+    border-radius: 10px;
+    border: #fff;
+    font-weight: bolder;
+  }
+  .btn-cover .page-count {
+    font-weight: bolder;
+    padding: 0 1rem;
   }
 
 </style>
