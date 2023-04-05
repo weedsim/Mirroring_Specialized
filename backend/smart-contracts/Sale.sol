fec5e0e958f70baafa826a1a1fd5ca50bfbe98d8
김성환 <fanngineer@gmail.com>
2023-04-05 21:45:08 +0900

    :sparkles: nft factory 추가 전

--- backend/smart-contracts/Sale.sol
+++ backend/smart-contracts/Sale.sol
@@ -8,7 +8,7 @@ import "contracts/NFT.sol";
 
 contract Sale is Ownable{
     address public admin; //관리자 계정
-    address private _nftContractAddress; //nft스마트 컨트랙트
+    address public nftContractAddress; //nft스마트 컨트랙트 주소
 
     // 생성자에 의해 정해지는 값
     address payable public seller; //판매자
@@ -16,7 +16,7 @@ contract Sale is Ownable{
     uint256 public tokenId; //판매NFT ID
     bool public isEnded; //완료여부
     bool public isCanceled; //취소여부
-    MyToken private _NFTContract;
+    MyToken public NFTContract;
     uint256 public endedAt; //판매시간
     address private _buyer;//구매자 지갑주소
 
@@ -26,27 +26,31 @@ contract Sale is Ownable{
         address payable _seller,
         uint256 _tokenId,
         uint256 _price,
-        address _nftContractAddress
+        address _nftContractAddress,
+        address _admin
     ) {
-        require(_price  >= 0, "Price must be higher than 0.");
+        require(_price >= 0, "Price must be higher than 0.");
         require(MyToken(_nftContractAddress).ownerOf(_tokenId) == _seller, "Seller is not owner.");
         tokenId = _tokenId;
         price = _price;
         seller = _seller;
         isEnded = false;
         isCanceled = false;
-        _NFTContract = MyToken(_nftContractAddress);
-        _NFTContract.approve(admin,_tokenId);
+        nftContractAddress = _nftContractAddress;
+        NFTContract = MyToken(_nftContractAddress);
+        // _NFTContract.approve(admin,_tokenId);
+        admin = _admin;
     }
 
     function purchase() payable public returns(bool) {
         require(!isEnded, "This sale is already ended.");
-        require(_NFTContract.ownerOf(tokenId) == seller);
+        require(NFTContract.ownerOf(tokenId) == seller);
         require(msg.value == price, "Buyer's balance is exhausted.");
         //판매자에게서 구매자의 Money전송
         seller.transfer(msg.value);
         _buyer = msg.sender;
         // end();
+        emit SaleEnded(msg.sender, price);
         return true;
     }
 
@@ -74,5 +78,4 @@ contract Sale is Ownable{
         require(isEnded, "This sale is not ended yet.");
         return endedAt;
     }
-
 }
--- backend/smart-contracts/SaleFactory.sol
+++ backend/smart-contracts/SaleFactory.sol
@@ -11,9 +11,10 @@ import "contracts/Sale.sol";
  */
 contract SaleFactory is Ownable {
     address public admin; //관리자 계정
-    address private _nftContractAddress; //nft스마트 컨트랙트
+    address public nftContractAddress; //nft스마트 컨트랙트
+    
     address[] public sales; //판매 정보 저장할 배열
-    uint256 private _saleIds; //판매 정보 인덱스
+    uint256 public saleIds; //판매 정보 인덱스
     
     event NewSale(
         address indexed _saleContract,
@@ -21,28 +22,33 @@ contract SaleFactory is Ownable {
         uint256 _workId
     );
 
-    constructor(address nftContractAddress) {
+    constructor(address _nftContractAddress) {
         admin = msg.sender;
-        _nftContractAddress = nftContractAddress;
+        nftContractAddress = _nftContractAddress;
     }
 
     /**
      * @dev 반드시 구현해야하는 함수입니다. 
      */
     function createSale(
-        uint256 itemId,
-        uint256 price
+        uint256 _tokenId,
+        uint256 _price
     ) public returns (address) {
         //생성될 판매정보의 인덱스
-        uint256 newSaleId = _saleIds;
-        _saleIds++;
+        // uint256 newSaleId = current();
         //판매정보 생성
-        Sale newSale = new Sale(payable(msg.sender), itemId, price* (10**18), _nftContractAddress);
+        Sale newSale = new Sale(payable(msg.sender), _tokenId, _price*(10**18), nftContractAddress, admin);
         //판매 정보 등록
         address saleContractAddress = address(newSale);
-        sales[newSaleId] = saleContractAddress;
-        emit NewSale(saleContractAddress, msg.sender, newSaleId);
+        sales.push(saleContractAddress);
+        emit NewSale(saleContractAddress, msg.sender, saleIds);
+        saleIds++;
+        return saleContractAddress;
     }
+
+    // function current() public view returns (uint256) {
+    //     return saleIds;
+    // }
 
     function allSales() public view returns (address[] memory) {
         return sales;
